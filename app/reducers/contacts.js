08fd68e6c31446ec6ba6556cfdac0b1e09d07f66
spencer <spencajones@gmail.com>
2018-03-29 07:06:23 -0600

    added more immutable.js code to reducers 'contacts'

--- app/reducers/contacts.js
+++ app/reducers/contacts.js
@@ -1,49 +1,37 @@
 // @flow
-
+import { Map } from 'immutable';
 import { FETCHING_CONTACTS, FETCHING_CONTACTS_FAILURE, FETCHING_CONTACTS_SUCCESS, REMOVE_FETCHING_CONTACTS } from '../../app/actions/contacts';
-
-
-type actionType = {
-  +type: string,
-  +error: string,
-  +importedContacts?: mixed
-};
+import type { actionType } from '../utils/constants';
 
 type contactStateType = {
   +isFetching: boolean,
   +error: string
 };
 
-export const initialState = {
+export const initialState = Map({
   isFetching: false,
   error: '',
-};
+});
 
 export default function contacts(state: contactStateType = initialState, action: actionType) {
   switch (action.type) {
     case FETCHING_CONTACTS:
-      return {
-        ...state,
+      return state.merge({
         isFetching: true,
-      };
+      });
     case FETCHING_CONTACTS_FAILURE:
-      return {
-        ...state,
+      return state.merge({
         error: action.error,
         isFetching: false,
-      };
+      });
     case FETCHING_CONTACTS_SUCCESS:
-      return {
-        ...state,
-        isFetching: false,
-        ...action.importedContacts
-      };
+      return state.merge(Map({ isFetching: false }))
+        .merge(action.importedContacts);
     case REMOVE_FETCHING_CONTACTS:
-      return {
-        ...state,
+      return state.merge({
         isFetching: false,
         error: '',
-      };
+      });
     default:
       return state;
   }
--- test/reducers/contacts.spec.js
+++ test/reducers/contacts.spec.js
@@ -1,48 +1,47 @@
-import contacts from '../../app/reducers/contacts'
-import * as actions from '../../app/actions/contacts'
+import { Map } from 'immutable';
+import contacts from '../../app/reducers/contacts';
+import * as actions from '../../app/actions/contacts';
 
 describe('contacts reducer:', () => {
   describe('has an initial state', () => {
     it('initial state is setup correctly', () => {
-      expect(contacts(undefined, {})).toEqual({
+      expect(contacts(undefined, {})).toEqual(Map({
         isFetching: false,
         error: '',
-      })
-    })
-  })
+      }));
+    });
+  });
   describe('can handle fetches', () => {
     it('initiates fetch', () => {
-       expect(contacts(undefined, actions.fetchingContacts())).toEqual({
-         isFetching: true,
-         error: '',
-       })
-    })
+      expect(contacts(undefined, actions.fetchingContacts())).toEqual(Map({
+        isFetching: true,
+        error: '',
+      }));
+    });
     it('handles a success', () => {
-      const fakeContacts = {
+      const fakeContacts = Map({
         1234: {
           id: 1234,
           name: 'cornflower',
         }
-      }
-      
-      expect(contacts(undefined, actions.fetchingContactsSuccess(fakeContacts))).toEqual({
+      });
+    expect(contacts(undefined, actions.fetchingContactsSuccess(fakeContacts))).toEqual(Map({
         isFetching: false,
         error: '',
-        ...fakeContacts,
-      })
-    })
+      }).merge(fakeContacts));
+    });
     it('handles a failure', () => {
-      const err = 'Error Fetching Contacts'
-      expect(contacts(undefined, actions.fetchingContactsFailure(err))).toEqual({
+      const err = 'Error Fetching Contacts';
+      expect(contacts(undefined, actions.fetchingContactsFailure(err))).toEqual(Map({
         isFetching: false,
         error: err,
-      })
-    })
+      }));
+    });
     it('can remove a fetch', () => {
-      expect(contacts({isFetching: true, error: ''}, actions.removeFetchingContacts())).toEqual({
+      expect(contacts(Map({ isFetching: true, error: '' }), actions.removeFetchingContacts())).toEqual(Map({
         isFetching: false,
         error: '',
-      })
-    })
-  })
-})
+      }));
+    });
+  });
+});
