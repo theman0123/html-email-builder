e6a0ae10309f821cdfa1ed5b77fc427ab6819715
spencer <spencajones@gmail.com>
2018-04-30 16:18:56 -0600

    tried to get burger menu to show icon and not be overlay initially. needs to show icon, and add click anywhere function that closes menu.

--- app/components/Navigation/Navigation.js
+++ app/components/Navigation/Navigation.js
@@ -1,23 +1,30 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {decorator as reduxBurgerMenu} from 'redux-burger-menu/immutable';
+import { slide as Menu } from 'react-burger-menu';
+import {action as toggleMenu} from 'redux-burger-menu/immutable';
 import * as styles from './styles.css';
-import { slide as Menu } from 'react-burger-menu'
 
 type Props = {};
 
-export default class Navigation extends React.Component {
+class Navigation extends React.Component {
   showSettings (event) {
     event.preventDefault();
   }
 
   render () {
+    const NewMenu = reduxBurgerMenu(Menu);
     return (
-      <Menu className={styles.mainMenu} itemListClassName={styles.itemList} right noOverlay >
-        <div className={styles.header}>{'Select an App'}</div>
-        <Link to="/build-invoices">Build Invoices</Link>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-      </Menu>
+      <div className={styles.button}>
+        <NewMenu right className={styles.mainMenu} itemListClassName={styles.itemList} >
+          <div className={styles.header}>{'Select an App'}</div>
+          <Link to="/build-invoices">Build Invoices</Link>
+          <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+        </NewMenu>
+      </div>
     );
   }
 }
+
+export default Navigation;
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,13 @@
 // @flow
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
+import {reducer as burgerMenu} from 'redux-burger-menu/immutable';
 import counter from './counter';
 import user from './user';
 
 const rootReducer = combineReducers({
   user,
+  burgerMenu,
   counter,
   router,
 });
