97f1b27971214ad2ad914dd213c8d0d4c963fc9d
spencer <spencajones@gmail.com>
2018-05-03 16:14:07 -0600

    added tests for Navigation. BurgerMenu displays on right of screen and is clickable, minimizes as well.

--- app/components/Navigation/Navigation.js
+++ app/components/Navigation/Navigation.js
@@ -1,28 +1,42 @@
 // @flow
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import {decorator as reduxBurgerMenu} from 'redux-burger-menu/immutable';
+import { decorator as reduxBurgerMenu } from 'redux-burger-menu/immutable';
 import { slide as Menu } from 'react-burger-menu';
-import {action as toggleMenu} from 'redux-burger-menu/immutable';
+import { action as toggleMenu } from 'redux-burger-menu/immutable';
 import * as styles from './styles.css';
 
 type Props = {};
 
 class Navigation extends React.Component {
-  showSettings (event) {
+  showSettings(event) {
     event.preventDefault();
   }
 
-  render () {
-    const NewMenu = reduxBurgerMenu(Menu);
+  render() {
+    const BurgerMenu = reduxBurgerMenu(Menu);
     return (
-      <div className={styles.button}>
-        <NewMenu right className={styles.mainMenu} itemListClassName={styles.itemList} >
-          <div className={styles.header}>{'Select an App'}</div>
-          <Link to="/build-invoices">Build Invoices</Link>
-          <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-        </NewMenu>
-      </div>
+      <BurgerMenu
+        right
+        burgerButtonClassName={styles.burgerButton}
+        burgerBarClassName={styles.burgerBars}
+        crossButtonClassName={styles.crossButton}
+        crossClassName={styles.cross}
+        menuClassName={styles.burgerMenu}
+        itemListClassName={styles.itemList}>
+        <div className={styles.menuHeader}>
+          {'Select an App'}
+        </div>
+        <Link to="/build-invoices">
+          {'Build Invoices'}
+        </Link>
+        <a
+          onClick={ this.showSettings }
+          className="menu-item--small"
+          href="">
+          {'Settings'}
+        </a>
+      </BurgerMenu>
     );
   }
 }
--- test/containers/Navigation.spec.js
+++ test/containers/Navigation.spec.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer'
+import { Navigation } from '../../app/components';
+import { action as toggleMenu } from 'redux-burger-menu/immutable';
+import { slide as Menu } from 'react-burger-menu';
+import { decorator as reduxBurgerMenu } from 'redux-burger-menu/immutable';
+import { configureStore, history } from '../../app/store/configureStore';
+
 
 describe('Navigation container', () => {
-  it('should received object from redux that lists possible routes/menu items');
-  it('menu items are linked to appropriate routes');
-  it('when click targets non navigation container, navigation should minimize');
-  it('navigation should open on click of icon');
-});
+  describe('Navigation icon should be visible', () => {
+//    const store = configureStore();
+    
+    it('Match previous visible styling', () => {
+      const wrapper = shallow(<Navigation />);
+//     figure out a way to test if component is visible -> look at testing css console.log(wrapper.find('.burgerButtonMenu'), styles.burgerButtonClassName)
+        expect(wrapper.props()).toMatchSnapshot();
+    });
+  });
+  describe('store.burgerMenu represents bugerMenu component', () => {
+    it('should toggle menu open', () => {
+      const store = configureStore();
+      const closed = store.getState().burgerMenu.get('isOpen');
 
-// small exmaple below//
-//describe('something', () => {
-//  let wrapper;
-//  describe('something', () => {
-//    beforeEach(() => {
-//      wrapper = shallow(<Component/>);
-//
-//      it('something', () => {
-//        expect(wrapper.find('onClick')).toHaveLength(1);
-//      });
-//    });
-//  });
-//});
+      store.dispatch(toggleMenu(true));
+
+      const open = store.getState().burgerMenu.get('isOpen');
+      const closedThenOpen = closed === false && open === true;
+
+      expect(closedThenOpen).toEqual(true);
+    });
+    it('menu items are linked to appropriate routes');
+  });
+});
+// should render menu, click on 'html-invoices' and see redux store change to that location //
